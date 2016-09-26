6172095d828d5a7b721181c62027e08359b13740
remythemsen <remeeh@gmail.com>
2016-09-27 00:51:20 +0200

    implementing parser

--- src/main/scala/IO/Parser.scala
+++ src/main/scala/IO/Parser.scala
@@ -1,8 +1,33 @@
 package IO
+import scala.io.Source
+import scala.collection.mutable.ListBuffer
 
 /**
   * Created by remeeh on 9/26/16.
   */
 class Parser {
 
+  def parseInput(fileName:String) = {
+    val data = Source.fromFile(fileName).getLines.toList
+    convertToTuples(data)
+  }
+
+  def convertToTuples[A](l:List[A]) = {
+    var id:String = "";
+    var res:ListBuffer[(String, List[Double])] = ListBuffer[(String, List[Double])]()
+
+    l.zipWithIndex.collect {
+      case (e,i) => {
+        if (((i+1) % 2) == 1) {
+          id = e.toString
+        } else {
+          // Add new tuple to list
+          res += Tuple2(id, e.toString.split(" ").map(x => x.toDouble).toList)
+        }
+      }
+    }
+
+    // return result
+    res.toList
+  }
 }
