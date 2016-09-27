3655f95e0825c69d3c7a1fd791e19817c5caebc9
remythemsen <remeeh@gmail.com>
2016-09-27 22:05:44 +0200

    sketching out First Cluster analysis method

---
+++ src/main/scala/dataAnalysis/ClusterAnalysis.scala
@@ -0,0 +1,12 @@
+package dataAnalysis
+
+/**
+  * Created by remeeh on 9/27/16.
+  */
+object ClusterAnalysis {
+  def distAnalysis = {
+    // Get distances from p to all other in P
+    // Divide all distances into bins (decide bin size)
+    // Plot resulting histogram
+  }
+}
