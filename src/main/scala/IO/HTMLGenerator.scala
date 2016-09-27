c9f81a14cd21c3c5b398c1f17aa3b3320bcdfa76
remythemsen <remeeh@gmail.com>
2016-09-27 19:35:55 +0200

    finishing HTML generator

--- src/main/scala/IO/HTMLGenerator.scala
+++ src/main/scala/IO/HTMLGenerator.scala
@@ -13,16 +13,30 @@ object HTMLGenerator {
   }
 
   def output(resultSet:List[(String,List[Double])], path:String, fileName:String) : Unit = {
-    //TODO Implement this.
-    // open html document
 
-    // convert tuple to image
+    val sb = new StringBuilder
+    // HEADER
+    sb.append("<!doctype html>")
+    sb.append("\n<html lang=\"en\">")
+    sb.append("\n<head>")
+    sb.append("\n<meta chartset=\"utf-8\">")
+    sb.append("<title>The HTML5 Herald</title>")
+    // triple double-quotes because of scala string interpolation bug
+    sb.append(s"""<link rel=\"stylesheet\" href=\"$path/styles.css?v=1.0\">""")
+    sb.append("</head>")
+    // BODY
+    sb.append("<body>")
+    for(result <- resultSet) {
+      sb.append(imgTag(result._1))
+    }
 
-    // close html document
+    sb.append("</body>")
+    sb.append("</html>")
 
-    val content = "HELLOFILE"
 
-    writeToFile(s"$path/$fileName", content)
+    writeToFile(s"$path/$fileName", sb.mkString)
   }
+  // triple double-quotes because of scala string interpolation bug
+  private def imgTag(id:String) = s"""<img src=\"http://disa.fi.muni.cz/profimedia/images/$id\" />"""
 
 }
