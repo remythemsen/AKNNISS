2a75b73fa657f2c4943ee0ecfd06d4c9b0f254a9
remythemsen <remeeh@gmail.com>
2016-09-27 19:55:01 +0200

    adding IO for test

--- src/main/scala/Program.scala
+++ src/main/scala/Program.scala
@@ -7,5 +7,12 @@ object Program {
     val hello = new demo.Hello
     println(hello.sayHello("Hi from Main"))
 
+
+    // Getting from file
+    val data = IO.Parser.parseInput(getClass.getResource("descriptors-mini.data").getPath)
+
+    // Outputting to file
+    IO.HTMLGenerator.outPut(data, "", "index.html")
+
   }
 }
