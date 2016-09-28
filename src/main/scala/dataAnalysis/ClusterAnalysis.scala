9eeef0eb5557b3ef1c04265678b1681164612f96
remythemsen <remeeh@gmail.com>
2016-09-28 16:40:02 +0200

    Adding collecting distances mechanism

--- src/main/scala/dataAnalysis/ClusterAnalysis.scala
+++ src/main/scala/dataAnalysis/ClusterAnalysis.scala
@@ -1,12 +1,34 @@
 package dataAnalysis
 
+import scala.collection.mutable.ListBuffer
+
 /**
   * Created by remeeh on 9/27/16.
   */
 object ClusterAnalysis {
-  def distAnalysis = {
+  def distAnalysis(data:List[(String, List[Double])]) = {
+    // Convert to array for quick random access and size
+    val tuples = data.toVector
+
+    // Pick a random Point
+    val rnd = scala.util.Random
+    val rp = tuples(rnd.nextInt(tuples.length))
+
+    var distances = ListBuffer[Double]()
+
+
     // Get distances from p to all other in P
+    for(t <- tuples) {
+      distances.insert(distTo(rp._2, t._2))
+    }
+
     // Divide all distances into bins (decide bin size)
+
+
+
     // Plot resulting histogram
+  }
+  def distTo(p1:List[Double], p2:List[Double]) = {
+    1
   }
 }
