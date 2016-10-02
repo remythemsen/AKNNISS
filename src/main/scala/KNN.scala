d85f83009525b435c91eb1c75f694e6a0d19e4cf
remythemsen <remeeh@gmail.com>
2016-10-02 18:35:26 +0200

    Implementing exact KNN

--- src/main/scala/KNN.scala
+++ src/main/scala/KNN.scala
@@ -1,6 +1,24 @@
 /**
   * Created by remeeh on 9/27/16.
   */
-class KNN {
+import LSH.Distance
+
+object KNN {
+  def findKNearest(q:(String,Vector[Double]), k:Int, tuples:List[(String, Vector[Double])]) : List[(String, Double)] = {
+    // Measure distances from q to each other tuple
+    val distances = for {
+      t <- tuples
+    } yield (t._1, Distance.euclideanDistance(q._2, t._2))
+
+    // Take out first k, return candidate-set
+    distances.sortBy(_._2).take(k)
+  }
+
+  def precision() = {
+    // Run LSH m times with random q points, return resultset
+    // Hamming distance compare result from knearest, and result from LSH
+    //
+  }
+
 
 }
