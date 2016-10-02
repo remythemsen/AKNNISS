942750c2553707ff8c104293562d0fc9bdf439ca
remythemsen <remeeh@gmail.com>
2016-10-02 04:22:47 +0200

    Finishing up printing out file with generated histogram

--- src/main/scala/dataAnalysis/ClusterAnalysis.scala
+++ src/main/scala/dataAnalysis/ClusterAnalysis.scala
@@ -1,6 +1,10 @@
 package dataAnalysis
 
+
 import LSH.Distance
+import breeze.plot._
+import breeze.linalg.linspace
+
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 /**
@@ -11,6 +15,21 @@ object ClusterAnalysis {
     // Convert to array for quick random access and size
     val tuples = data.toVector
 
+
+    // Plot resulting histogram
+    val f = Figure()
+    val p = f.subplot(0)
+    for(a <- 0 until 16) {
+      p += hist(getDistancesFromRandomPoint(tuples),100)
+    }
+
+    p.ylabel = "# of Points"
+    p.xlabel = "Normalized Distance from P"
+    f.saveas("lines.png")
+
+  }
+  def transformIntoRange(x:Double, min:Double, max:Double) = (x-min)/(max-min) // change to std normalization
+  def getDistancesFromRandomPoint(tuples:Vector[(String, Vector[Double])]) : Vector[Double] = {
     // Pick a random Point
     val rnd = scala.util.Random
     val rp = tuples(rnd.nextInt(tuples.length))
@@ -24,66 +43,8 @@ object ClusterAnalysis {
     val sortedDs = distances.sorted.tail // skip head since that will be q point
 
     // normalize
-    val nDistances = for {
+    for {
       d <- sortedDs
-    } yield (transformIntoRange(d, sortedDs.head, sortedDs(sortedDs.size-1), 0.0, 1.0))
-
-    // divide into buckets and count
-    val numberOfBuckets = 100;
-
-    val buckets = ArrayBuffer[Int]()
-    for(a <- 0 until numberOfBuckets) {
-      buckets+=(0)
-    }
-
-    for(x <- nDistances) {
-      val bucket = findBucket(x, numberOfBuckets-1)
-      buckets.update(bucket, buckets(bucket) + 1)
-    }
-
-    // Plot resulting histogram
-
-
-    for (x <- buckets) {
-      println(x)
-    }
-  }
-
-/*
-    // Divide all distances into bins (decide bin size)
-    // Map to range 0.0 - 1.0 (affine transformation)
-    // Get max
-    val sortedDistances = distances.sorted
-    val max = sortedDistances.last
-    // Get min
-    println(sortedDistances.apply(0))
-    val min = sortedDistances.apply(0) // TODO: Find instead min of dataset
-    // tranform into range 0.0 - 1.0
-    val transformedSortedDistances = sortedDistances.map(x => transformIntoRange(x, min, max, 0.0, 1.0))
-
-
-    // Plot resulting histogram
-    val numberOfBuckets = 100;
-
-    val buckets = ArrayBuffer[Int]()
-    for( a <- 0 to numberOfBuckets-1 ) {
-      buckets+=(0)
-    }
-
-    for(x <- transformedSortedDistances) {
-      println(x)
-      val bucket = findBucket(x, numberOfBuckets)
-      buckets.update(bucket, buckets(bucket) + 1)
-    }
-
-
-
-  }
-  */
-  def findBucket(x:Double, n:Int) : Int = {
-    Math.floor(x * (n)).toInt
-  }
-  def transformIntoRange(x:Double, min:Double, max:Double, newMin:Double, newMax:Double) : Double = {
-    (x-min)/(max-min) // change to std normalization
+    } yield (transformIntoRange(d, sortedDs.head, sortedDs(sortedDs.size-1)))
   }
 }
