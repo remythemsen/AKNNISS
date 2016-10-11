22266dfdb2f2cf7de9283fe21af0b3b424d23639
remythemsen <remeeh@gmail.com>
2016-10-11 02:25:27 +0200

    Shifting from lists to iterator and stream

--- src/main/scala/IO/Parser.scala
+++ src/main/scala/IO/Parser.scala
@@ -1,43 +1,21 @@
 package IO
+import scala.collection.immutable.Stream.cons
 import scala.io.Source
-import scala.collection.mutable.ListBuffer
 
 /**
   * Created by remeeh on 9/26/16.
   */
 object Parser {
-
-  def parseInput(fileName:String) = {
-    val data = Source.fromFile(fileName).getLines
-    convertToTuples(data)
+  def parseInput(fileName:String) : Stream[(String, Vector[Double])] = {
+    convertToTuples(Source.fromFile(fileName).getLines())
   }
-
-/*  def parseInputUntil(fileName:String, n:Int) = {
-    val data = Source.fromFile(fileName).getLines().take(n)
-    convertToTuples(data)
-  }*/
-
-  // a sample of 666 gives 99% confidence
-  // that mean is +/- 5% from population
-  def getDistributedSample(sampleSize:Int) = {}
-
-
-  private def convertToTuples[A](l:Iterator[String]) = {
-    var id:String = "";
-    var res:ListBuffer[(String, Vector[Double])] = ListBuffer[(String, Vector[Double])]()
-
-    l.zipWithIndex.collect {
-      case (e,i) => {
-        if (((i+1) % 2) == 1) {
-          id = e.toString.substring(49)
-        } else {
-          // Add new tuple to list
-          res += Tuple2(id, e.toString.split(" ").map(x => x.toDouble).toVector)
-        }
-      }
+  // TODO Make sure its tail recursive
+  def convertToTuples(data: Iterator[String]) : Stream[(String, Vector[Double])] = {
+    if(data.hasNext) {
+      val set = data.take(2).toList
+      cons(Tuple2(set(0).toString.substring(49), set(1).toString.split(" ").map(x => x.toDouble).toVector), convertToTuples(data))
+    } else {
+      Stream.Empty
     }
-
-    // return result
-    res.toList
   }
 }
