e3041b4a873c1f84db086f8740dd340de25d42c5
remythemsen <remeeh@gmail.com>
2016-10-12 17:25:50 +0200

    Updating input type

--- src/main/scala/dataAnalysis/ClusterAnalysis.scala
+++ src/main/scala/dataAnalysis/ClusterAnalysis.scala
@@ -6,37 +6,44 @@ import breeze.plot._
   * Created by remeeh on 9/27/16.
   */
 object ClusterAnalysis {
-  def distAnalysis(data:List[(String, Vector[Double])], dist:Distance) = {
-    // Convert to vector for quick random access and size
-    val tuples = data.toVector
-
+  def distAnalysis(data:Stream[(String, Vector[Double])], dist:Distance, k:Int, b:Int) : Figure = {
 
     // Plot resulting histogram
     val f = Figure()
     val p = f.subplot(0)
-    for(a <- 0 until 16) {
-      p += hist(getDistancesFromRandomPoint(tuples, dist),100)
-    }
-
     p.ylabel = "# of Points"
     p.xlabel = "Normalized Distance from P"
-    f.saveas("lines.png")
 
+    @annotation.tailrec
+    def loop(i:Int = 0) : Unit = {
+      if(i != k) {
+        p+= hist(getDistancesFromRandomPoint(data, dist),b)
+        loop(i+1)
+      }
+    }
+    loop(0)
+
+    // Return figure object
+    f
   }
+
   def normalize(x:Double, min:Double, max:Double) = (x-min)/(max-min) // change to std normalization
-  def getDistancesFromRandomPoint(tuples:Vector[(String, Vector[Double])], dist:Distance) : Vector[Double] = {
-    // Pick a random Point
+
+  def getDistancesFromRandomPoint(tuples:Stream[(String, Vector[Double])], dist:Distance) : Vector[Double] = {
+    // TODO Efficiently Grab a Random "Chunk" of size 600-ish
     val rnd = scala.util.Random
+
+    // TODO Select Random Tuple from chunk
     //val rp = tuples(rnd.nextInt(tuples.length))
     val rp = tuples(5)
 
-    // Compute distances from rp to rest
+    // Compute distances from rp to rest of chunk
     // Map to range 0.0 - 1.0 (affine transformation)
     val distances = for {
       t <- tuples
     } yield (dist.measure(rp._2, t._2))
 
-    val sortedDs = distances.sorted.tail // skip head since that will be q point
+    val sortedDs = distances.toVector.sorted.tail
 
     // normalize
     for {
