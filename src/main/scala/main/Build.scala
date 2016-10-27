6ce703a442c2e533c4db25059509b2a019f18f13
remythemsen <remeeh@gmail.com>
2016-10-27 17:02:55 +0200

    finishing up builder

--- src/main/scala/main/Build.scala
+++ src/main/scala/main/Build.scala
@@ -1,51 +1,125 @@
 package main
 
+import java.io.{File, FileOutputStream, ObjectOutputStream}
+import IO.Parser
+import LSH.hashFunctions.{CrossPolytope, HashFunction, Hyperplane}
+import LSH.structures.HashTable
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.pattern.{ask, pipe}
 import akka.util.Timeout
-
+import preProcessing.DimensionalityReducer
 import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 
 object Build {
 
+  // TODO Deal with cases of success and failures
   implicit val timeout = Timeout(3.hours)
 
-  class Aggregator extends Actor {
-    def receive = {
-      case _ => {
-        println("Starting A")
+  case class BuildTables(timeOut:Timeout)
+  case class ConfigBuild(data: File = new File("."), outDir: String = ".", functions:Int = 17, tables:Int = 4, hashFunction:String = "Hyperplane")
 
-        // Remember the await stop when all has returned success,
-        // or one has returned failure
-        val futs = new ArrayBuffer[Future[Any]]()
-        for(i <- 1 to 5) {
-          val c = context.actorOf(Props(new C), "c"+i)
-          futs += c ? ""
-        }
-        val res = Await.result(Future.sequence(futs), timeout.duration)
-        println("all is finsished")
-        println(res)
-      }
+  def main(args: Array[String]) = {
+    val parser = new scopt.OptionParser[ConfigBuild]("build") {
+      head("AKNNISS Build", "0.x")
+
+      opt[File]('d', "data").required().valueName("<file>").
+        action( (x, c) => c.copy(data = x) ).
+        text("data to generate LSH Structure from")
+
+      opt[String]('o', "outdir").required().valueName("<path>").
+        action( (x, c) => c.copy(outDir = x) ).
+        text("dir to store generated LSHStructure")
+
+      opt[Int]('k', "functions").action( (x, c) =>
+        c.copy(functions = x) ).text("Number of Hashfunctions")
+
+      opt[Int]('L', "tables").action( (x, c) =>
+        c.copy(tables = x) ).text("Number of Hashtables\n")
+
+      opt[String]('h', "hashfunction").action( (x, c) =>
+        c.copy(hashFunction = x) ).text("Hashfunction to use\n")
+
+      help("help").text("prints this usage text\n\n")
+
+      note("Approximate K-Nearest Neighbor Image Similarity Search\nCreated by Roxana, Remy and Chris, Fall 2016")
+
     }
 
-    class C extends Actor {
-      def receive = {
-        case _ => {
-          println("running "+context.self.path)
-          sender ! 5
+    // parser.parse returns Option[C]
+    parser.parse(args, ConfigBuild()) match {
+      case Some(config) =>
+        val hashFC:() => HashFunction = {
+          if (config.hashFunction.equals("Hyperplane"))
+            () => new Hyperplane(config.functions)
+          else if (config.hashFunction.equals("Crosspolytope"))
+            () => new CrossPolytope(config.functions)
+          else {
+            throw new Exception("Unknown Hash Function")
+          }
         }
-      }
+        // Save LSHStructure to file.
+        val dir:String = config.outDir.concat("/")
+          // constructing filename
+          .concat(config.hashFunction)
+          .concat("_")
+          .concat(config.functions.toString)
+          .concat("_")
+          .concat(config.tables.toString)
+          .concat(".lshstructure")
+
+
+        val system = ActorSystem("LSHStructureBuilder")
+        val sb = system.actorOf(Props(new StructureBuilder(config.tables, config.functions, hashFC, config.data)), "sb")
+        val lshStructure = Await.result(sb ? BuildTables(Timeout(5.hours)), Timeout(5.hours).duration)
+        system.terminate()
+
+        val fis = new FileOutputStream(dir.toString)
+        val oos = new ObjectOutputStream(fis)
+        oos.writeObject(lshStructure)
+        oos.close
+
+      case None =>
+        // arguments are bad, error message will have been displayed
+        println("Invalid Arguments")
     }
   }
-  def main(args: Array[String]) = {
-    val system = ActorSystem("LSHStructureBuilder")
-    val sb = system.actorOf(Props(new Aggregator), "sb")
-    sb ! "lol"
-    system.terminate()
-    println("lol")
+
+  class StructureBuilder(l:Int, k:Int, hf:()=>HashFunction, data:File) extends Actor {
+    case object BuildTable
+    def receive = {
+      case BuildTables(timeOut) => {
+        implicit val timeout = timeOut
+        val futs = new ArrayBuffer[Future[Any]]()
+        for(i <- 0 until l) {
+          val c = context.actorOf(Props(new TableBuilder(k, hf, data)), "c"+i)
+          futs += c ? BuildTable
+        }
+        val res = Await.result(Future.sequence(futs), timeout.duration)
+        sender ! res
+      }
+    }
+
+    class TableBuilder(k:Int, hf: () => HashFunction, data:File) extends Actor {
+      val parser = new Parser(data)
+      def receive = {
+        case BuildTable => {
+          val table = new HashTable(hf)
+          var j = 0
+          val size = parser.size
+          while(j < size) {
+            j = j+1
+
+            var elem = parser.next
+            val reduced = (elem._1, DimensionalityReducer.getNewVector(elem._2, parser.size, parser.vLength))
+            table+=(reduced)
+          }
+          sender ! table
+        }
+      }
+    }
   }
 }
