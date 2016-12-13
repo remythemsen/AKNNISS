3f9e0ebd6c7b51afe3c391bfa352b3df11751cb0
Roxana Larisa Dobre <roxana.larisa.dobre@gmail.com>
2016-12-13 20:24:03 +0100

    Fixes on the query generator

--- performance/src/main/scala/KNN.scala
+++ performance/src/main/scala/KNN.scala
@@ -6,9 +6,6 @@ import utils.tools.Distance
 import scala.collection.mutable
 import scala.math.Ordering
 
-/**
-  * Created by chm on 12/13/2016.
-  */
 class KNN(k:Int){
   // TODO: pass number of probes for each CP
   var maxHeap = new mutable.PriorityQueue[(Int,Float)]()(Ord)
@@ -17,18 +14,18 @@ class KNN(k:Int){
     def compare(x:(Int,Float), y:(Int,Float)) = x._2.compare(y._2)
   }
 
-  def findKNearest(q:(Int,Array[Float]), k:Int,fileName:String ,distance:Distance) : mutable.PriorityQueue[(Int, Float)] = {
+  def findKNearest(q:(Int,Array[Float]), k:Int,fileName:String, distance:Distance): mutable.PriorityQueue[(Int, Float)] = {
 
     val data = new ReducedFileParser(new File(fileName))
 
     // Take out first k, return candidate-set
-    var i=0
+    var i = 0
     while(data.hasNext){
-      while(i<30){
+      while(i < 30){
         maxHeap.enqueue((data.next._1,distance.measure(q._2,data.next._2)))
         i+=1
       }
-      if(distance.measure(q._2,data.next._2)<maxHeap.head._2){
+      if(distance.measure(q._2,data.next._2) < maxHeap.head._2){
         maxHeap.dequeue()
         maxHeap.enqueue((data.next._1,distance.measure(q._2,data.next._2)))
       }
--- querygenerator/src/test/scala/QuerySampleGeneratorTest.scala
+++ querygenerator/src/test/scala/QuerySampleGeneratorTest.scala
@@ -3,20 +3,17 @@ import java.io.File
 import IO.DisaFileParser
 import utils.IO.ReducedFileParser
 
-/**
-  * Created by chm on 12/13/2016.
-  */
 object QuerySampleGeneratorTest {
 
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
 
     val queryParserSample = new ReducedFileParser(new File("querySampleFile2.data"))
-    val arrayOfSamples=new Array[(Int,Array[Float])](queryParserSample.size)
+    val arrayOfSamples = new Array[(Int,Array[Float])](queryParserSample.size)
 
     var i=0
     while(queryParserSample.hasNext){
-      arrayOfSamples(i)=(queryParserSample.next)
+      arrayOfSamples(i) = (queryParserSample.next)
       i+=1
     }
 
