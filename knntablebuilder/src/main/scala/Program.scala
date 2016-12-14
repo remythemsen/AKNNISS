ba75a347951145257c106f9e52777286ea6c89b8
remythemsen <remeeh@gmail.com>
2016-12-14 17:30:07 +0100

    making knntablebuilder multithreaded

--- knntablebuilder/src/main/scala/Program.scala
+++ knntablebuilder/src/main/scala/Program.scala
@@ -1,11 +1,15 @@
 import java.io.{File, FileOutputStream, ObjectOutputStream}
-
+import scala.concurrent.duration._
 import utils.IO.ReducedFileParser
 import utils.tools.{Cosine, Distance}
 
-import scala.collection.mutable
+import scala.concurrent.ExecutionContext.Implicits.global
 
-case class Config(buildFromFile:String, queries:String, outPath:String, n:Int, qn:Int, knn:Int, measure:Distance)
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.{Await, Future}
+
+case class Config(buildFromFile:String, queries:String, outPath:String, n:Int, qn:Int, knn:Int, distance:Distance)
 
 object Program extends App {
 
@@ -14,7 +18,7 @@ object Program extends App {
       "./knntablebuilder/data/query-261.data",  // Q File
       "./knntablebuilder/data",                   // Out path
       39286,                      // N
-      161,                        // Queries
+      258,                        // Queries
       30,                         // KNN
       Cosine)                     // MEASURE
 
@@ -24,18 +28,42 @@ object Program extends App {
 
   println("Building Structure")
   var progress = 0.0
-  var percentile = config.qn / 100
+  var percentile = config.n / 100
 
-  while(queries.hasNext) {
-    var q = queries.next
-    var knnFinder = new KNN
+  var priorityQueues = new Array[mutable.PriorityQueue[(Int, Float)]](config.qn)
+  for(p <- 0 until config.qn) {
+    priorityQueues(p) = new mutable.PriorityQueue[(Int, Float)]
+  }
 
+  var loadedQueries = new Array[(Int, Array[Float])](config.qn)
+  for(i <- 0 until loadedQueries.length) {
+    loadedQueries(i) = queries.next
+  }
 
-    structure += (q._1 -> knnFinder.findKNearest(q, config.knn, config.buildFromFile, config.measure))
+  while(data.hasNext) {
+    var futures = new ArrayBuffer[Future[Unit]]
+    var dataPoint = data.next
+    for(i <- 0 until loadedQueries.length) {
+      var q = loadedQueries(i)
+      futures += Future {
+        val pqEntry = (q._1, config.distance.measure(dataPoint._2, q._2))
+        if(priorityQueues(i).size <= 30) {
+          priorityQueues(i).enqueue(pqEntry)
+        }
+        else {
+          if(priorityQueues(i).head._2 > pqEntry._2) {
+            priorityQueues(i).dequeue()
+            priorityQueues(i).enqueue(pqEntry)
+          }
+        }
+      }
+    }
+    Await.result(Future.sequence(futures), 20 seconds)
+
     progress+=1
 
     if(progress % percentile == 0) {
-      println(((progress / config.qn) * 100).toInt.toString + "%")
+      println(((progress / config.n) * 100).toInt + "%")
     }
   }
 
