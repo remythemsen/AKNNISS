8a5920976aa4bc81807d8c6ac3089837f0e96f6a
Roxana <roxana.larisa.dobre@gmail.com>
2016-12-19 16:59:02 +0100

    removed the 3-step probing

--- LSH/src/main/scala/LSH/multiprobing/MultiProbingHyperplane.scala
+++ LSH/src/main/scala/LSH/multiprobing/MultiProbingHyperplane.scala
@@ -15,17 +15,19 @@ class MultiProbingHyperplane(hashcode: Array[Int]){
   //generating 1-step, 2-step and 3-step probing buckets
   def generateProbes(): ArrayBuffer[Array[Int]] = {
     val listBuckets = new ArrayBuffer[Array[Int]]()
+    // 1-step probing
     for(i <- 0 until M){
       var newCode = hcode
       newCode = hcode.updated(i, flipSign(hcode(i)))
       listBuckets += newCode
+      // 2-step probing
        for(j <- i+1 until M){
          newCode = newCode.updated(j, flipSign(newCode(j)))
          listBuckets += newCode
-         for(k <- j+1 until M){
-           newCode = newCode.updated(k, flipSign(newCode(k)))
-           listBuckets += newCode
-         }
+//         for(k <- j+1 until M){
+//           newCode = newCode.updated(k, flipSign(newCode(k)))
+//           listBuckets += newCode
+//         }
        }
     }
     listBuckets
