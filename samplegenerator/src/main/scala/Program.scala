a5eb3321f066e7d50ee100a5fbb7dad30f2123da
chrismatrakos <chmat@itu.dk>
2016-12-22 02:50:57 +0100

    created samplegenerator module to fix/rewrite file of vectors < 256 d

---
+++ samplegenerator/src/main/scala/Program.scala
@@ -0,0 +1,8 @@
+
+
+object Program extends App {
+  val pathToFile="data/descriptors-decaf-1m.data"
+  val dir = "data/"
+  SampleGenerator.generateSampleFile(pathToFile, dir)
+
+}
---
+++ samplegenerator/src/main/scala/SampleGenerator.scala
@@ -0,0 +1,51 @@
+import java.io.{BufferedWriter, File, FileWriter}
+
+import utils.IO.ReducedFileParser
+
+import scala.util.Random
+
+object SampleGenerator {
+
+  var outFile:File = _
+  var bw:BufferedWriter = _
+
+  def generateSampleFile(fileName: String, outDir: String): Unit={
+
+    // hardcoded change with variable fileName
+    val data = new ReducedFileParser(new File(fileName))
+
+    var index = 0.0
+    val percentile = data.size / 100
+    var sampleSize = 0
+    outFile = new File(outDir + "descriptors-file-fixed-"+data.size+".data")
+
+    bw = new BufferedWriter(new FileWriter(outFile))
+
+    while(data.hasNext){
+      var tmpTuple = data.next
+
+      if (tmpTuple._2.size == 256) {
+        // Write to file
+        var sb = new StringBuilder
+        sb.append(tmpTuple._1)
+        sb.append(" ")
+        for (component <- tmpTuple._2) {
+          sb.append(component + " ")
+        }
+        sb.append("\n")
+
+        // Write resulting set
+        bw.write(sb.toString())
+        sampleSize += 1
+      }
+      // update progress by 1
+      index += 1
+      if(index % percentile == 0) {
+        println(((index / data.size) * 100).toInt.toString + "%")
+      }
+    }
+    bw.close()
+
+    println("finished file with a total of "+ sampleSize +" samples")
+  }
+}
